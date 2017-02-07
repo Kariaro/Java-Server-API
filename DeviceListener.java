29f323a50e4816da7b24f46f643c6022812f4c8c
Kariaro <victoraxberg@gmail.com>
2017-02-07 20:17:20 +0100

    Delete DeviceListener.java

--- DeviceListener.java
+++
@@ -1,7 +0,0 @@
-package scripters.api.network;
-
-public interface DeviceListener {
-	public void clientConnection(DeviceEvent event);
-	public void clientDisconnect(DeviceEvent event);
-	public void clientTimeout(DeviceEvent event);
-}
